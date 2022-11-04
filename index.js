841c1e5c0aac813dbf6551a57aacec40c4cbd0e8
MJigah <jigah4july@gmailcom>
2022-11-04 21:35:53 +0100

    addinng changes

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const arithRoutes = require('./routes/arithmeticRoutes')
-const swaggerUI = require('swagger-ui-express')
-const swaggerJsDoc = require('swagger-jsdoc')
+const arithRoutes = require("./routes/arithmeticRoutes");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-app.use(express.json());
-
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Auth API',
-            version: '1.0.0', 
-            description: 'Simple Calculation API'
-        },
-        servers: [
-            {
-                url: 'http://localhost:3000/'
-            }
-        ],
-        components: {
-            securitySchemes:
-            {
-                bearerAuth:{
-                    type: 'http',
-                    scheme: 'bearer',
-                    bearerFormat: 'JWT'
-                }
-            }
-        },
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Auth API",
+      version: "1.0.0",
+      description: "Simple Calculation API",
     },
-    apis: ["./routes/*.js"]
-}
+    servers: [
+      {
+        url: "http://localhost:3000/",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+  },
+  apis: ["./routes/*.js"],
+};
+const specs = swaggerJsDoc(options);
 
-const specs = swaggerJsDoc(options)
-
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs))
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+app.use(
+    cors({
+      origin: "*",
+    })
+  );
+  app.use(express.json());
 app.use("/api", arithRoutes);
 
 app.get("/favicon.ico", function (req, res) {
